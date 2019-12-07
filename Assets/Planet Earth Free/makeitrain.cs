fc16f99e6895092a87ad1c824eabc5f5475f7f91
Mehdi <meddu94@live.fr>
2019-12-07 19:40:49 +0100

    added a new enlarge script
    added a new enlarge script that enlarge the planet regarding a given factor

---
+++ Assets/Planet Earth Free/makeitrain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeitrain : MonoBehaviour {
+
+    [Tooltip("How quick your bool needs to be enlarged")]
+    public float enlargeFactor = 0.001f;
+
+    public struct Scale
+    {
+        public Vector3 vec;
+        public void SetCoordinate(Vector3 newVec) {
+            vec = newVec;
+        }
+    }
+    // Start is called before the first frame update
+    Scale coordinate = new Scale();
+
+    void Start()
+    {
+        coordinate.SetCoordinate(transform.localScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coordinate.vec += new Vector3(enlargeFactor, enlargeFactor, enlargeFactor);
+        transform.localScale = coordinate.vec;
+    }
+}
